cea01cef2978e505bd2f879861f63627a2f757c4
Dineth988 <dilmindineth988@gmail.com>
2025-02-25 15:53:07 +0530

    login db

--- firebase.js
+++ firebase.js
@@ -2,8 +2,9 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js"; // 🔥 Import Firebase Auth
+  
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,5 +22,7 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   const db = getFirestore(app); 
+  const auth = getAuth(app); // 🔥 Initialize Firebase Auth
 
-  export { db };
+  export { db, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
+ 
